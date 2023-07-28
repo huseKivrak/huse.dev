d3c17fdf978fc7cf85d7989487cbb71c752237f4
Huse Kivrak <huse7@pm.me>
2023-07-28 17:47:32 -0400

    edit text

--- lib/content.tsx
+++ lib/content.tsx
@@ -3,7 +3,7 @@ import { Project } from "./types";
 export const PROJECTS: Project[] = [
   {
     title: "Jobly",
-    description: "A full stack job search web application",
+    description: "a full stack job search web application",
     image: "/images/jobly.png",
     url: "https://jobly.huse.dev",
     github: "https://github.com/husekivrak/jobly",
@@ -12,7 +12,7 @@ export const PROJECTS: Project[] = [
   {
     title: "Warbler",
     image: "/images/warbler.png",
-    description: "A Twitter clone",
+    description: "a social media app",
     url: "https://warbler-huse.onrender.com",
     github: "https://github.com/husekivrak/warbler",
     tags: ["Flask", "PostgreSQL", "SQLAlchemy"],
@@ -20,7 +20,7 @@ export const PROJECTS: Project[] = [
   {
     title: "RithmGo",
     image: "/images/rithmgo.jpg",
-    description: "A full stack web application for learning Go",
+    description: "a mobile app for students",
     github: "https://github.com/husekivrak/rithmgo",
     tags: ["React Native", "TypeScript"],
   },
