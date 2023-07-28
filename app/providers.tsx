83f285edd66a3e87efbb85580c6ea945a703c204
Huse Kivrak <huse7@pm.me>
2023-07-28 13:41:50 -0400

    added nextui

---
+++ app/providers.tsx
@@ -0,0 +1,12 @@
+// provider for nextui
+'use client'
+
+import {NextUIProvider} from '@nextui-org/react'
+
+export function Providers({children}: { children: React.ReactNode }) {
+  return (
+    <NextUIProvider>
+      {children}
+    </NextUIProvider>
+  )
+}
