d25117119c094e467c1a38e8f6fa2d900087ccd6
Huse Kivrak <huse7@pm.me>
2023-07-29 15:46:15 -0400

    added tech icons and style experimenting

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,18 @@
+import { IconContext } from "react-icons";
+import TechStack from "@/components/TechStack";
 
-export default function About():JSX.Element {
-    return (
-        <div id="about">
-            <h2 className="text-stone-100 text-4xl font-extralight tracking-widest col-span-full">about</h2>
-
-        </div>
-    )
+export default function About(): JSX.Element {
+  return (
+    <div id="about">
+      <h2 className="text-stone-100 text-4xl font-extralight tracking-widest">
+        about
+      </h2>
+      <div id="tech">
+        <h4 className="text-stone-100 text-3xl font-extralight tracking-widest">
+          tech
+        </h4>
+        <TechStack />
+      </div>
+    </div>
+  );
 }
