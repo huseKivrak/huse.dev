67848fc7f5a16d532ca03d85f3d916764cab92f5
Huse Kivrak <huse7@pm.me>
2023-07-30 18:53:36 -0400

    site restyling

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={jose.className}>
-      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+      <body className="antialiased max-w-xl mb-35 flex flex-col md:flex-row mx-4 mt-4 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Nav />
           {children}
