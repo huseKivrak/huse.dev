e628d2aedd3199bad799bd2b06d778b1e6b3be9a
Huse Kivrak <huse7@pm.me>
2023-07-30 18:54:24 -0400

    MPA revamp

---
+++ app/about/page.tsx
@@ -0,0 +1,17 @@
+import TechStack from "@/components/TechStack";
+export default function About(): JSX.Element {
+  return (
+    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 font-light">
+      <h2 className=" text-4xl font-extralight  col-span-full">
+        about
+      </h2>
+
+      <div id="tech" className="my-2">
+        <h4 className=" text-3xl font-light tracking-widest">tech</h4>
+        <p className="font-thin text-2xl text-stone-50"></p>
+        <p className="font-thin text-xl text-stone-50"></p>
+        <TechStack />
+      </div>
+    </section>
+  );
+}
---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer(): JSX.Element {
+  return (
+    <footer className="flex flex-col items-center justify-center w-full h-24">
+      <p className=" text-white font-light tracking-widest text-xl">
+        ©2023 huse kivrak
+      </p>
+    </footer>
+  );
+}
