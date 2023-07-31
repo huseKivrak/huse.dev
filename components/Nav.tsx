5a7db9aab1375f3a9117bd6cda1f21a209ddaff9
Huse Kivrak <huse7@pm.me>
2023-07-30 22:52:22 -0400

    reverting, active links had performance issues

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,4 @@
 "use client";
-import clsx from "clsx";
 import { NAV_LINKS } from "@/lib/content";
 import {
   NavigationMenu,
@@ -14,25 +13,16 @@ import {
   DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
+
 import Link from "next/link";
 import { SlMenu } from "react-icons/sl";
-import { usePathname } from "next/navigation";
-
 export default function Nav() {
-  const pathname = usePathname();
-
   return (
     <div className="bg-transparent p-0 font-light tracking-widest max-w-xl sticky ">
       <NavigationMenu>
         <NavigationMenuList className="md:flex hidden justify-start">
           <NavigationMenuItem className="mr-4 ml-1 font-normal">
-            <Link
-              className={clsx(
-                "hover:text-stone-500",
-                pathname !== "/" && "font-light"
-              )}
-              href="/"
-            >
+            <Link className="hover:text-stone-500" href="/">
               home
             </Link>
           </NavigationMenuItem>
@@ -40,10 +30,8 @@ export default function Nav() {
             {NAV_LINKS.map((link, index) => (
               <NavigationMenuItem key={index} className="mx-4">
                 <Link
-                  className={clsx(
-                    "hover:text-stone-500",
-                    pathname === link.url && "text-white font-normal"
-                  )}
+                  className="hover:text-stone-500"
+                  color="foreground"
                   href={link.url}
                 >
                   {link.name}
@@ -59,13 +47,12 @@ export default function Nav() {
 }
 
 const HamburgerMenu = () => {
-  const pathname = usePathname();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
           className="md:hidden p-0 -ml-3 inline-flex items-center rounded-md bg-transparent hover:text-stone-100 hover:bg-stone-700"
-          variant="ghost"
+          variant="default"
           size="icon"
         >
           <SlMenu />
@@ -79,10 +66,7 @@ const HamburgerMenu = () => {
         <DropdownMenuItem key={"home-link"} asChild>
           <Link
             href="/"
-            className={clsx(
-              "hover:text-stone-500 ",
-              pathname !== "/" && "font-light text-stone-100"
-            )}
+            className="text-stone-200 bg-transparent hover:text-stone-600"
           >
             home
           </Link>
@@ -91,11 +75,8 @@ const HamburgerMenu = () => {
         {NAV_LINKS.map((link, index) => (
           <DropdownMenuItem key={`${link.name}-${index}-${link.name}`} asChild>
             <Link
-              className={clsx(
-                "hover:text-stone-500 text-stone-100 font-light",
-                pathname === link.url && "text-white font-normal"
-              )}
               href={link.url}
+              className=" text-stone-200 bg-transparent hover:text-stone-600 "
               key={`${link.name}-${index}`}
             >
               {link.name}
