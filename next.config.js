3cafae450d5ad67a821a64c3cb9e7ae583de5957
Huse Kivrak <huse7@pm.me>
2023-07-31 13:16:52 -0400

    proper syntax

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
-\
+const nextConfig = {
+}
+
 
 module.exports = {
-
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-}
+  nextConfig,
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+  }
