8a9597701a7785c92dcdba699a1e6182b3bbda8a
Huse Kivrak <huse7@pm.me>
2023-08-01 19:59:50 -0400

    more style tinkering

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Josefin_Sans } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
-import Nav from "@/components/Nav";
-import Footer from "@/components/Footer";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Josefin_Sans } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
+import Nav from '@/components/Nav';
+import Footer from '@/components/Footer';
 
 const jose = Josefin_Sans({
-  subsets: ["latin"],
-  display: "swap",
+  subsets: ['latin'],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
-  title: "huse kivrak",
-  description: "Portfolio of Huse Kivrak",
+  title: 'huse kivrak',
+  description: 'Portfolio of Huse Kivrak',
 };
 
 export default function RootLayout({
@@ -21,15 +21,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={jose.className}>
-      <body className="antialiased max-w-xl mb-35 flex flex-col md:flex-row mx-4 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-8 flex flex-col px-2 md:px-0">
+    <html lang='en' className={jose.className}>
+      <body className='antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 lg:mx-auto'>
+        <main className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
           <Nav />
           {children}
           <Analytics />
         </main>
       </body>
-        <Footer />
+      <Footer />
     </html>
   );
 }
