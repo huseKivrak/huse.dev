443049f0cc695fafc4cc6b8955c0626d222d3b07
Huse Kivrak <huse7@pm.me>
2023-08-01 20:01:56 -0400

    removed unneeded slugAsParam, may add back

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -52,10 +52,6 @@ export const Post = defineDocumentType(() => ({
       type: 'string',
       resolve: (doc) => `/${doc._raw.flattenedPath}`,
     },
-    slugAsParams: {
-      type: 'string',
-      resolve: (doc) => doc._raw.flattenedPath.split('/').slice(1).join('/'),
-    },
   },
 }));
 
