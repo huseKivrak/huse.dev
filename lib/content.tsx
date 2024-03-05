288cdde2ce3d6bff9eb9eba75332a1b70c73ed7c
Huse Kivrak <huse7@pm.me>
2024-03-05 11:25:38 -0500

    removed site, moved jobly up

--- lib/content.tsx
+++ lib/content.tsx
@@ -1,56 +1,49 @@
-import { Project } from "./types";
+import {Project} from './types';
 
 /** NAV LINKS */
 export const NAV_LINKS = [
-
-  {
-    name: "projects",
-    url: "/projects",
-  },
-  {
-    name: "blog",
-    url: "/blog",
-  },
-  // {
-  //   name: "about",
-  //   url: "/about",
-  // },
-  // {
-  //   name: "contact",
-  //   url: "/contact",
-  // },
+	{
+		name: 'projects',
+		url: '/projects',
+	},
+	{
+		name: 'blog',
+		url: '/blog',
+	},
+	// {
+	//   name: "about",
+	//   url: "/about",
+	// },
+	// {
+	//   name: "contact",
+	//   url: "/contact",
+	// },
 ];
 
 /** PROJECTS */
 export const PROJECT_LIST: Project[] = [
-  {
-    title: "Warbler",
-    image: "/images/projects/warbler.png",
-    description: "a social media application",
-    url: "https://warbler-huse.onrender.com",
-    github: "https://github.com/husekivrak/warbler",
-    tags: ["Flask", "PostgreSQL", "SQLAlchemy"],
-  },
-  {
-    title: "Jobly",
-    description: "a job search web application",
-    image: "/images/projects/jobly.png",
-    url: "https://jobly.huse.dev",
-    github: "https://github.com/husekivrak/jobly",
-    tags: ["Express.js", "React", "PostgreSQL"],
-  },
-  {
-    title: "RithmGo",
-    image: "/images/projects/rithmgo.jpg",
-    description: "a mobile application for students",
-    github: "https://github.com/husekivrak/rithmgo",
-    tags: ["React Native", "TypeScript"],
-  },
-  {
-    title: "huse.dev",
-    image: "/images/projects/website.png",
-    description: "my personal website",
-    github: "https://github.com/husekivrak/huse.dev",
-    tags: ["Next.js", "TypeScript", "Vercel", "Tailwind CSS"],
-  }
+	{
+		title: 'Jobly',
+		description: 'a job search web application',
+		image: '/images/projects/jobly.png',
+		url: 'https://jobly.huse.dev',
+		github: 'https://github.com/husekivrak/jobly',
+		tags: ['Express.js', 'React', 'PostgreSQL'],
+	},
+	{
+		title: 'Warbler',
+		image: '/images/projects/warbler.png',
+		description: 'a social media application',
+		url: 'https://warbler-huse.onrender.com',
+		github: 'https://github.com/husekivrak/warbler',
+		tags: ['Flask', 'PostgreSQL', 'SQLAlchemy'],
+	},
+
+	{
+		title: 'RithmGo',
+		image: '/images/projects/rithmgo.jpg',
+		description: 'a mobile application for students',
+		github: 'https://github.com/husekivrak/rithmgo',
+		tags: ['React Native', 'TypeScript'],
+	},
 ];
