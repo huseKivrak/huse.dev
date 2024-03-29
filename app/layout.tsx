b3682899221585f4355ca50884b6747c589023b1
Huse Kivrak <huse7@pm.me>
2024-03-29 15:36:08 -0400

    testing UserGuiding install code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,39 @@
 import './globals.css';
-import type { Metadata } from 'next';
-import { Josefin_Sans } from 'next/font/google';
+import type {Metadata} from 'next';
+import {Josefin_Sans} from 'next/font/google';
 
 import Nav from '@/components/Nav';
 import Footer from '@/components/Footer';
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import {SpeedInsights} from '@vercel/speed-insights/next';
 
 const jose = Josefin_Sans({
-  subsets: ['latin'],
-  display: 'swap',
+	subsets: ['latin'],
+	display: 'swap',
 });
 
 export const metadata: Metadata = {
-  title: 'huse kivrak',
-  description: "Huse Kivrak's personal website",
+	title: 'huse kivrak',
+	description: "Huse Kivrak's personal website",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang='en' className={jose.className}>
-      <body className='antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 lg:mx-auto'>
-        <main className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
-          <Nav />
-          {children}
-          <SpeedInsights/>
-        </main>
-      </body>
-      <Footer />
-    </html>
-  );
+export default function RootLayout({children}: {children: React.ReactNode}) {
+	return (
+		<html lang='en' className={jose.className}>
+			<head>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `(function(g,u,i,d,e,s){g[e]=g[e]||[];var f=u.getElementsByTagName(i)[0];var k=u.createElement(i);k.async=true;k.src='https://static.userguiding.com/media/user-guiding-'+s+'-embedded.js';f.parentNode.insertBefore(k,f);if(g[d])return;var ug=g[d]={q:[]};ug.c=function(n){return function(){ug.q.push([n,arguments])};};var m=['previewGuide','finishPreview','track','identify','hideChecklist','launchChecklist'];for(var j=0;j<m.length;j+=1){ug[m[j]]=ug.c(m[j]);}})(window,document,'script','userGuiding','userGuidingLayer','4ZI80797D76ID');`,
+					}}
+				/>
+			</head>
+			<body className='antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 lg:mx-auto'>
+				<main className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
+					<Nav />
+					{children}
+					<SpeedInsights />
+				</main>
+			</body>
+			<Footer />
+		</html>
+	);
 }
