f1631f30576d005251564b6d0b09ff6e3adc6b1c
Huse Kivrak <huse7@pm.me>
2024-03-29 15:41:12 -0400

    testing UserGuiding with next/script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import type {Metadata} from 'next';
 import {Josefin_Sans} from 'next/font/google';
-
+import Script from 'next/script';
 import Nav from '@/components/Nav';
 import Footer from '@/components/Footer';
 import {SpeedInsights} from '@vercel/speed-insights/next';
@@ -20,11 +20,9 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 	return (
 		<html lang='en' className={jose.className}>
 			<head>
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `(function(g,u,i,d,e,s){g[e]=g[e]||[];var f=u.getElementsByTagName(i)[0];var k=u.createElement(i);k.async=true;k.src='https://static.userguiding.com/media/user-guiding-'+s+'-embedded.js';f.parentNode.insertBefore(k,f);if(g[d])return;var ug=g[d]={q:[]};ug.c=function(n){return function(){ug.q.push([n,arguments])};};var m=['previewGuide','finishPreview','track','identify','hideChecklist','launchChecklist'];for(var j=0;j<m.length;j+=1){ug[m[j]]=ug.c(m[j]);}})(window,document,'script','userGuiding','userGuidingLayer','4ZI80797D76ID');`,
-					}}
-				/>
+				<Script>
+					{`(function(g,u,i,d,e,s){g[e]=g[e]||[];var f=u.getElementsByTagName(i)[0];var k=u.createElement(i);k.async=true;k.src='https://static.userguiding.com/media/user-guiding-'+s+'-embedded.js';f.parentNode.insertBefore(k,f);if(g[d])return;var ug=g[d]={q:[]};ug.c=function(n){return function(){ug.q.push([n,arguments])};};var m=['previewGuide','finishPreview','track','identify','hideChecklist','launchChecklist'];for(var j=0;j<m.length;j+=1){ug[m[j]]=ug.c(m[j]);}})(window,document,'script','userGuiding','userGuidingLayer','4ZI80797D76ID');`}
+				</Script>
 			</head>
 			<body className='antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 lg:mx-auto'>
 				<main className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
