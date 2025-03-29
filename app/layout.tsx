9ef321f0565348be02010d444d852eec311b8390
Huse Kivrak <huse7@pm.me>
2025-03-29 01:56:55 -0400

    removed old components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Josefin_Sans } from 'next/font/google';
-import Nav from '@/components/Nav';
-import Footer from '@/components/Footer';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const jose = Josefin_Sans({
@@ -18,14 +16,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en' className={jose.className}>
-			<body className='antialiased max-w-7xl mb-40 flex flex-col md:flex-row mx-4 lg:mx-auto'>
+			<body className='antialiased'>
 				<main className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
-					<Nav />
 					{children}
 					<SpeedInsights />
 				</main>
 			</body>
-			<Footer />
 		</html>
 	);
 }
